43d5825395b165ad0e0b2d5512ab161019822b8d
gusio11gk12 <91569176+gusio11gk12@users.noreply.github.com>
2021-10-12 19:03:35 -0500

    Hola mundo en Python
    Primer hola mundo en Python :)

---
+++ hellopython.py
@@ -0,0 +1,8 @@
+#  Hola mundo en Python
+#  Titulo: Hola Mundo en Python
+#  Description: Hola mundo en Python desde vim en windows
+#  Author: Gustavo Garcia Barrera
+#  Version 1
+#  Uso: c: \> python /path/hellopython.py
+print ("Hola mundo Python desde VIM")
+
