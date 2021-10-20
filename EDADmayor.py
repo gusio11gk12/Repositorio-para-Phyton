a884c2cc653a1150fc37077249fe837ba820594e
gusio11gk12 <91569176+gusio11gk12@users.noreply.github.com>
2021-10-19 20:11:47 -0500

    problema mayor de edad
    problema mayor edad en python

---
+++ EDADmayor.py
@@ -0,0 +1,18 @@
+
+#################################
+# Fecha: 19-10-2021             #
+# Autor: Gustavo Garcia Barrera #
+# <gus201711@gmail.com          #
+#################################
+
+
+fecha_Nacimiento = int (input("Digite su fecha de nacimiento: "))
+diferencia =int (2021 - fecha_Nacimiento)
+
+if diferencia >= 18:
+     print ("El usuario tiene", diferencia ," es ayorr de edad")
+else:
+    print ("El usuario tiene", diferencia ," es menor de edad")
+
+    
+
